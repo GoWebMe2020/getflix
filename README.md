743c7503077b8a940b9472852688502a74c52504
EC2 Default User <ec2-user@ip-172-31-13-213.eu-west-2.compute.internal>
2021-04-22 11:49:42 +0000

    Initial commit of files

---
+++ README.md
@@ -0,0 +1,1 @@
+Practice by building a NETFLIX replica
---
+++ js.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 0) {
+            $(".navbar").addClass("navbar-scroll");
+        } else {
+            $(".navbar").removeClass("navbar-scroll");
+        }
+    });
+});
